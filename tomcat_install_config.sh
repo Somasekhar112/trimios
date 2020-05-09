679f85ecce77de4744c4694ae37573d8fa8cb969
Somasekhar112 <59610564+Somasekhar112@users.noreply.github.com>
2020-05-09 11:11:56 +0530

    Update tomcat_install_config.sh

--- tomcat_install_config.sh
+++ tomcat_install_config.sh
@@ -4,9 +4,11 @@ yum install openjdk-1.8
 
 java -version
 
-vi /etc/profile {#!/bin/sh          export JAVA_HOME=/opt/java            export PATH=$JAVA_HOME/bin:$PATH  }
+vi /etc/profile     #!/bin/sh          export JAVA_HOME=/opt/java            export PATH=$JAVA_HOME/bin:$PATH  
 
 javac -version
+
+cd /opt
 
 wget https://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz
 
@@ -34,6 +36,14 @@ cd META-INF
 
 nano context.xml
 
-vi /etc/systemd/system/tomcat.service
+vi /etc/systemd/system/tomcat.service 
+
+sudo systemctl daemon-reload
+
+sudo systemctl start tomcat
+
+sudo systemctl enable tomcat
+
+systemctl status tomcat
 
 
